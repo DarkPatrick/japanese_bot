e79dd01782cbf03adaaaeccb453c7622c746cf93
Egor Semin <e.semin@expf.ru>
2022-11-13 21:50:12 +0600

    adding rows + returning full table

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,12 +1,15 @@
 import psycopg2
 import config as cfg
+import pandas as pd
 
 
-def connect() -> psycopg2.extensions.cursor:
+def connect() -> tuple[psycopg2.extensions.connection, psycopg2.extensions.cursor]:
     try:
-        conn = psycopg2.connect(host=cfg.postgre["host"], port=cfg.postgre["port"],
-                            database=cfg.postgre["default_database"], user=cfg.postgre["user"],
-                            password=cfg.postgre["password"])
+        conn = psycopg2.connect(host=cfg.postgre["host"],
+                                port=cfg.postgre["port"],
+                                database=cfg.postgre["default_database"],
+                                user=cfg.postgre["user"],
+                                password=cfg.postgre["password"])
         conn.autocommit = True
         with conn.cursor() as cur:
             cfg.logger.info("проверка существования бд")
@@ -23,27 +26,42 @@ def connect() -> psycopg2.extensions.cursor:
             conn.close()
     
     try:
-        conn = psycopg2.connect(host=cfg.postgre["host"], port=cfg.postgre["port"],
-                            database=cfg.postgre["database"], user=cfg.postgre["user"],
-                            password=cfg.postgre["password"])
+        conn = psycopg2.connect(host=cfg.postgre["host"],
+                                port=cfg.postgre["port"],
+                                database=cfg.postgre["database"],
+                                user=cfg.postgre["user"],
+                                password=cfg.postgre["password"])
         conn.autocommit = True
-        with conn.cursor() as cur:
-            cfg.logger.info("проверка существования таблицы")
-            cur.execute(f"select 1 from information_schema.tables where table_name='{cfg.postgre['datatable']}'")
-            is_exists = cur.fetchone()
-            if not is_exists:
-                cfg.logger.info("таблица ещё не существует, создаю...")
-                cur.execute(f"CREATE TABLE {cfg.postgre['datatable']} (word varchar, translate varchar);")
-            return cur
+        cur = conn.cursor()
+        cfg.logger.info("проверка существования таблицы")
+        cur.execute(f"select 1 from information_schema.tables where table_name='{cfg.postgre['datatable']}'")
+        is_exists = cur.fetchone()
+        if not is_exists:
+            cfg.logger.info("таблица ещё не существует, создаю...")
+            cur.execute(f"CREATE TABLE {cfg.postgre['datatable']} (word varchar, translate varchar);")
+        return conn, cur
     except psycopg2.Error as e:
         cfg.logger.info(f"ошибка обращения к таблице: {e}")
-        return None
-    finally:
         if conn:
             conn.close()
+        return None
 
 
-def add_row() -> int:
-    cur = connect()
+
+def add_row(word: str, translation: str) -> None:
+    conn, cur = connect()
+    cur.execute(f"insert into {cfg.postgre['datatable']} values('{word}', '{translation}')")
+    conn.close()
+
+def get_datatable() -> pd.DataFrame:
+     conn, cur = connect()
+     cur.execute(f"select * from {cfg.postgre['datatable']}")
+     df = pd.DataFrame(cur.fetchall(), columns=['word', 'translation'])
+     conn.close()
+     return df
 
 
+if __name__ == "__main__":
+    add_row("test1", "test2")
+    df = get_datatable()
+    print(df)
